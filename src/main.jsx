1d54e52578748016932c7603c54b3e930395fa69
Your NameTitín <titin.colomer.romero@gmail.com>
2022-12-23 16:45:45 +0100

    primer commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import BankApp from './BankApp';
 import { store } from './store/';
 import './styles/index.scss';
@@ -9,9 +9,9 @@ import './styles/index.scss';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={ store }>
-      <BrowserRouter>
+    <HashRouter>
         <BankApp />
-      </BrowserRouter>
+        </HashRouter>
     </Provider>
   </React.StrictMode>,
 )
